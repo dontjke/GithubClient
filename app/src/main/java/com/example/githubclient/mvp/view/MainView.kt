c83b5b52a5dbcd380d393172fbe8044620ccb713
Romajke <skuaall@mail.ru>
2023-06-12 18:49:53 +0300

    refactor MainView.kt

--- app/src/main/java/com/example/githubclient/mvp/view/MainView.kt
+++ app/src/main/java/com/example/githubclient/mvp/view/MainView.kt
@@ -5,7 +5,4 @@ import moxy.viewstate.strategy.AddToEndSingleStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
 @StateStrategyType(AddToEndSingleStrategy::class)
-interface MainView : MvpView {
-    fun init()
-    fun updateList()
-}
+interface MainView : MvpView
