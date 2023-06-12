3c3338f0370dae7713366a3845d28fecb2723d40
Romajke <skuaall@mail.ru>
2023-06-12 18:12:42 +0300

    add App, implementation cicerone

---
+++ app/src/main/java/com/example/githubclient/ui/App.kt
@@ -0,0 +1,21 @@
+package com.example.githubclient.ui
+
+import android.app.Application
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.Router
+
+class App : Application() {
+    companion object {
+        lateinit var instance: App
+    }
+    //Временно до даггера положим это тут
+    private val cicerone: Cicerone<Router> by lazy {
+        Cicerone.create()
+    }
+    val navigatorHolder get() = cicerone.getNavigatorHolder()
+    val router get() = cicerone.router
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
