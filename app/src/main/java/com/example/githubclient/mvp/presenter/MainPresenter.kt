a4841df427acb23218ae509c74095cfa811f5880
Romajke <skuaall@mail.ru>
2023-06-12 18:51:02 +0300

    refactor MainPresenter.kt

--- app/src/main/java/com/example/githubclient/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/githubclient/mvp/presenter/MainPresenter.kt
@@ -1,44 +1,17 @@
 package com.example.githubclient.mvp.presenter
 
-import com.example.githubclient.mvp.model.GithubUser
-import com.example.githubclient.mvp.model.GithubUsersRepo
-import com.example.githubclient.mvp.presenter.list.IUserListPresenter
 import com.example.githubclient.mvp.view.MainView
-import com.example.githubclient.mvp.view.list.UserItemView
+import com.example.githubclient.navigation.IScreens
+import com.github.terrakok.cicerone.Router
 import moxy.MvpPresenter
 
-class MainPresenter(private val usersRepo: GithubUsersRepo) : MvpPresenter<MainView>() {
-
-    class UsersListPresenter : IUserListPresenter {
-        val users = mutableListOf<GithubUser>()
-        override var itemClickListener: ((UserItemView) -> Unit)? = null
-        /*get() = TODO("Not yet implemented")
-        set(value) {}*/
-
-        override fun bindView(view: UserItemView) {
-            val user = users[view.pos]
-            view.setLogin(user.login)
-        }
-
-        override fun getCount(): Int {
-            return users.size
-        }
-    }
-
-    val usersListPresenter = UsersListPresenter()
-
+class MainPresenter(val router: Router, val screens: IScreens) :
+    MvpPresenter<MainView>() {
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
-        viewState.init()
-        loadData()
-        usersListPresenter.itemClickListener = {
-            //TODO
-        }
+        router.replaceScreen(screens.users())
     }
-
-    private fun loadData() {
-        val users = usersRepo.getUsers()
-        usersListPresenter.users.addAll(users)
-        viewState.updateList()
+    fun backClicked() {
+        router.exit()
     }
 }
