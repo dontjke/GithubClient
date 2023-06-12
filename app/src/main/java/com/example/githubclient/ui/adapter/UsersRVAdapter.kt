36446465d2995520992578708639561594b5244b
Romajke <skuaall@mail.ru>
2023-06-12 16:40:45 +0300

    refactor UsersRVAdapter

--- app/src/main/java/com/example/githubclient/ui/adapter/UsersRVAdapter.kt
+++ app/src/main/java/com/example/githubclient/ui/adapter/UsersRVAdapter.kt
@@ -1,29 +1,40 @@
 package com.example.githubclient.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubclient.databinding.ItemUserBinding
+import com.example.githubclient.mvp.presenter.list.IUserListPresenter
 import com.example.githubclient.mvp.view.list.UserItemView
 
-class UsersRVAdapter() : RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
+class UsersRVAdapter(private val presenter: IUserListPresenter) :
+    RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
 
-
-    inner class ViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root), UserItemView {
+    inner class ViewHolder(private val binding: ItemUserBinding) :
+        RecyclerView.ViewHolder(binding.root), UserItemView {
         override var pos = -1
         override fun setLogin(text: String) {
-            TODO("Not yet implemented")
+            binding.tvLogin.text = text
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            ItemUserBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        ).apply {
+            itemView.setOnClickListener { presenter.itemClickListener?.invoke(this) }
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount() = presenter.getCount()
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        return presenter.bindView(holder.apply {
+            pos = position
+        })
     }
 }
