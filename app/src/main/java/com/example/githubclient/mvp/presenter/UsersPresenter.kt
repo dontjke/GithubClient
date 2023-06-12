6db60e3f9c8b11cf22cb12645ff5350741bf05af
Romajke <skuaall@mail.ru>
2023-06-12 18:19:02 +0300

    refactor UsersPresenter.kt

--- app/src/main/java/com/example/githubclient/mvp/presenter/UsersPresenter.kt
+++ app/src/main/java/com/example/githubclient/mvp/presenter/UsersPresenter.kt
@@ -1,4 +1,40 @@
 package com.example.githubclient.mvp.presenter
 
-class UsersPresenter {
+import com.example.githubclient.mvp.model.GithubUser
+import com.example.githubclient.mvp.model.GithubUsersRepo
+import com.example.githubclient.mvp.presenter.list.IUserListPresenter
+import com.example.githubclient.mvp.view.UsersView
+import com.example.githubclient.mvp.view.list.UserItemView
+import com.github.terrakok.cicerone.Router
+import moxy.MvpPresenter
+
+class UsersPresenter(val usersRepo: GithubUsersRepo, val router: Router)  :
+    MvpPresenter<UsersView>() {
+    class UsersListPresenter : IUserListPresenter {
+        val users = mutableListOf<GithubUser>()
+        override var itemClickListener: ((UserItemView) -> Unit)? = null
+        override fun getCount() = users.size
+        override fun bindView(view: UserItemView) {
+            val user = users[view.pos]
+            view.setLogin(user.login)
+        }
+    }
+    val usersListPresenter = UsersListPresenter()
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        viewState.init()
+        loadData()
+        usersListPresenter.itemClickListener = { itemView ->
+            //TODO: переход на экран пользователя c помощью router.navigateTo
+        }
+    }
+    private fun loadData() {
+        val users = usersRepo.getUsers()
+        usersListPresenter.users.addAll(users)
+        viewState.updateList()
+    }
+    fun backPressed(): Boolean {
+        router.exit()
+        return true
+    }
 }
