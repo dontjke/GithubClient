cd40f0b49a374d1bd7d4f02cf6392771ca7c68f5
Romajke <skuaall@mail.ru>
2023-06-12 16:25:28 +0300

    refactor UsersRVAdapter

--- app/src/main/java/com/example/githubclient/ui/adapter/UsersRVAdapter.kt
+++ app/src/main/java/com/example/githubclient/ui/adapter/UsersRVAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.githubclient.ui.adapter
 
-class UsersRVAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.githubclient.databinding.ItemUserBinding
+import com.example.githubclient.mvp.view.list.UserItemView
+
+class UsersRVAdapter() : RecyclerView.Adapter<UsersRVAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root), UserItemView {
+        override var pos = -1
+        override fun setLogin(text: String) {
+            TODO("Not yet implemented")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
